1bcca125b80c5c8f367bfcc3df36d5dcbab93a8d
cvlavanya <cvlavanya@gmail.com>
2012-10-16 23:18:13 -0400

    All warnings removed.

--- src/main/java/CasConsumer.java
+++ src/main/java/CasConsumer.java
@@ -20,14 +20,12 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
-
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.cas.FSIterator;
 import org.apache.uima.collection.CasConsumer_ImplBase;
 import org.apache.uima.collection.base_cpm.CasObjectProcessor;
-import org.apache.uima.examples.SourceDocumentInformation;
+//import org.apache.uima.examples.SourceDocumentInformation;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceConfigurationException;
--- src/main/java/CollectionReader.java
+++ src/main/java/CollectionReader.java
@@ -25,7 +25,7 @@ import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.collection.CollectionReader_ImplBase;
-import org.apache.uima.examples.SourceDocumentInformation;
+//import org.apache.uima.examples.SourceDocumentInformation;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.DocumentAnnotation;
 import org.apache.uima.resource.ResourceConfigurationException;
@@ -130,8 +130,7 @@ public class CollectionReader extends CollectionReader_ImplBase {
   /**
    * @see org.apache.uima.collection.CollectionReader#getNext(org.apache.uima.cas.CAS)
    */
-  @SuppressWarnings("deprecation")
-public void getNext(CAS aCAS) throws IOException, CollectionException {
+  public void getNext(CAS aCAS) throws IOException, CollectionException {
     JCas jcas;
     try {
       jcas = aCAS.getJCas();
